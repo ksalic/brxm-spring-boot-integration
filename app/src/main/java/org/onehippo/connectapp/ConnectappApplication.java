9f6f19a5b661dc923aa61c5d9aac586252f6d2b1
ksalic <kenan.salic@bloomreach.com>
2018-09-06 12:45:40 +0200

    initial setup

---
+++ app/src/main/java/org/onehippo/connectapp/ConnectappApplication.java
@@ -0,0 +1,12 @@
+package org.onehippo.connectapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConnectappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConnectappApplication.class, args);
+	}
+}
